5990dfa928a845d75d330ac65a690f30bbc0f052
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-06-11 16:36:59 -0500

    feat: afternoon push-up

---
+++ src/main/java/com/codeup/springblog/AdRepository.java
@@ -0,0 +1,10 @@
+package com.codeup.springblog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdRepository extends JpaRepository<Ad, Long> {
+
+    Ad findByTitle(String title);
+
+    Ad findByTitleLike(String searchPattern); //find by title "%INPUT%"
+}
