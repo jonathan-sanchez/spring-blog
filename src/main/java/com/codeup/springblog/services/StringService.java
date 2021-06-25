d920805aff6bf0dad5b7f19a6e3aeb5e17c54cf6
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-06-25 11:49:32 -0500

    feat: update

--- src/main/java/com/codeup/springblog/services/StringService.java
+++ src/main/java/com/codeup/springblog/services/StringService.java
@@ -1,4 +1,10 @@
 package com.codeup.springblog.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StringService {
+    public String shortenString(String originalString) {
+        return "short string";
+    }
 }
--- src/main/java/com/codeup/springblog/services/UserDetailsLoader.java
+++ src/main/java/com/codeup/springblog/services/UserDetailsLoader.java
@@ -1,4 +1,23 @@
 package com.codeup.springblog.services;
 
-public class UserDetailsLoader {
+import com.codeup.springblog.DAOs.UserRepository;
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.models.UserWithRoles;
+
+public class UserDetailsLoader implements UserDetailsService {
+    rivate final UserRepository users;
+
+    public UserDetailsLoader(UserRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = users.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
 }
